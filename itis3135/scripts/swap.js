be98c82f661b15165a63dd37620952d697005dde
rgalavi <89814147+rgalavi@users.noreply.github.com>
2021-10-28 00:51:51 -0400

    notes.js

--- itis3135/scripts/swap.js
+++ itis3135/scripts/swap.js
@@ -1,14 +1,14 @@
 
 $(document).ready(function() {
-
+    //attacj event handlers
     $(".img").click( evt => {
 
         const link = evt.currentTarget;
 
-        
+        //swap image
         $("#main-image").attr("src", link.src);
 
-        
+        //swap caption
         $("#caption").text(link.title);
     });
 
