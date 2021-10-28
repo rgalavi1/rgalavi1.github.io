7a3600a50cb3ff36b2f432a674089617aec11cff
rgalavi <89814147+rgalavi@users.noreply.github.com>
2021-10-28 00:49:53 -0400

    gallery

--- itis3135/scripts/swap.js
+++ itis3135/scripts/swap.js
@@ -1,24 +1,16 @@
 
 $(document).ready(function() {
 
-    $("#image-list a").each( (index, link) => {
-        const image = new Image();
-        image.src = link.href;
-    });
-    $("#image-list a").click( evt => {
+    $(".img").click( evt => {
+
         const link = evt.currentTarget;
 
-        $("#main-image").attr("src", link.href);
-
-        $("#caption").text(link.title);
-
-        evt.preventDefault();
-    });
-
-    $("li:first-child a").focus();
         
-    
- 
+        $("#main-image").attr("src", link.src);
+
+        
+        $("#caption").text(link.title);
+    });
 
 });
 
