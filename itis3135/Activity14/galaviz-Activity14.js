7b7d78126196b8c2079176bb1d22451a880cc8e4
rgalavi <89814147+rgalavi@users.noreply.github.com>
2021-11-17 16:12:23 -0500

    activity14,js

--- itis3135/Activity14/galaviz-Activity14.js
+++ itis3135/Activity14/galaviz-Activity14.js
@@ -1,22 +1,24 @@
 $(document).ready(function() {
     $("#nav_list li").click(function() {
         var title = $(this).children("a").attr("title");
-        var filename = title + "json";
-        consumeJson(filename);
+        var filename = title + ".json";
+        consumeJSON(filename);
     });
 	
 }); // end ready
 
-function consumeJson(jsonURL) {
+function consumeJSON(jsonFileURL) {
     $.ajax ({
-        url:"json_files/" + jsonURL,
+        url: "json_files" + jsonFileURL,
         dataType: "text",
         success: function(data) {
-            var json = $.parseJson(data);
-            $("main > h2").html(json.speakers[0].month + "<br/>" +json.speakers[0].speaker);
+            var json = $.parseJSON(data);
             $("main > h1").html(json.speakers[0].title);
-            $("main > h2").attr("src". json.speakers[0].image);
-            $("main > h2").html(json.speakers[0].text);
+            $("main > h2").html(json.speakers[0].month + "<br/>" +json.speakers[0].speaker);
+            $("main > h3").html(json.speakers[0].speaker);
+            $("main > img").attr("src", json.speakers[0].image);
+            $("main > p").html(json.speakers[0].text);
+            
 
         }
         
